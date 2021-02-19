82d3e6a3c5cf2aebdfd2cd41c0f420ccfca46fff
ArttuHeikkine <n0hear00@students.oamk.fi>
2021-02-19 16:27:37 +0200

    Weather

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import Weather from './Weather'
+
+function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsLoading(false);
+      }, (error) => {
+        alert(error);
+      })
+    } else {
+      alert('Your browser does not support geolocation!')
+    }
+  
+  }, [])
+  
+  if (isLoading) {
+    return<p>Loading...</p>;
+  } else {
+    return (
+    <div className="content">
+    <p>
+      Your position is {lat.toFixed(3)}, {lng.toFixed(3)} 
+    </p>
+    <Weather lat={lat} lng={lng} />
+    </div>
+    );
+  }
+}
+
+export default App;
